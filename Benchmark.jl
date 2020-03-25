82f5ab06b2efc02ab9a29423fbe6845d375c004e
Garben Tanghe <garben.tanghe@gmail.com>
2020-03-25 13:08:56 +0100

    added darknet-19 benchmark

--- Benchmark.jl
+++ Benchmark.jl
@@ -6,45 +6,47 @@ using CUDAdrv
 using CUDAnative
 using BenchmarkTools
 
-# define things to benchmark in a function
-function benchmark(a)
-    sin.(a);
-    cos.(a);
+# define what to benchmark
+function benchmark(i)
+    sin.(i);
+    cos.(i);
 end
 
-function profile(a)
+# define what to profile
+function profile(i)
     NVTX.mark("Started Profiling sin");
     NVTX.@range "Profiling sin" begin
-        sin.(a);
+        sin.(i);
     end
     NVTX.mark("Finished Profiling sin");
 
     NVTX.mark("Started Profiling cos");
     NVTX.@range "Profiling cos" begin
-        sin.(a);
+        cos.(i);
     end
     NVTX.mark("Finished Profiling cos");
 end
 
+# main
 function main(; benchmarking=true, profiling=true)
     # initialization
-    a = CuArrays.rand(1024,1024,128);
+    i = CuArrays.rand(1024,1024,128);
 
     # BENCHMARKIMG
     if (benchmarking == true)
         println("BENCHMARKING");
 
         # warmup
-        benchmark(a);
+        benchmark(i);
         # make sure the GPU is synchronized first
         CUDAdrv.synchronize();
 
         # get timing
         # '$', because external variables should be explicitly interpolated into the benchmark expression!
-        t = @benchmark CuArrays.@sync(benchmark($a)); #samples=100 seconds=10;
+        t = @benchmark CuArrays.@sync(benchmark($i)); #samples=100 seconds=10;
         print(IOContext(stdout, :compact => false), t);
         # get GPU allocations
-        CuArrays.@time benchmark(a) # synchronizes afterwards by itself
+        CuArrays.@time benchmark(i) # synchronizes afterwards by itself
     end
 
     # PROFILING
@@ -52,16 +54,16 @@ function main(; benchmarking=true, profiling=true)
         println("PROFILING");
 
         # warmup
-        profile(a);
+        profile(i);
         # make sure the GPU is synchronized first
         CUDAdrv.synchronize();
 
         # profile
-        CUDAdrv.@profile profile(a);
+        CUDAdrv.@profile profile(i);
     end
 end
 
-println("")
-println("")
-CUDAnative.device!(0)
-main(benchmarking=true, profiling=true)
+println("");
+println("");
+CUDAnative.device!(0);
+main(benchmarking=true, profiling=false);
