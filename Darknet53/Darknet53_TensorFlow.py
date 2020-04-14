16075b4f75081bafdf91aac08be32b95d8b0bdd1
Garben Tanghe <garben.tanghe@gmail.com>
2020-04-14 16:25:11 +0200

    bug fix for tensor addition

--- Darknet53/Darknet53_TensorFlow.py
+++ Darknet53/Darknet53_TensorFlow.py
@@ -1,13 +1,12 @@
 import time
 
 import numpy as np
-import nvtx.plugins.tf as nvtx_tf
+# import nvtx.plugins.tf as nvtx_tf
 import tensorflow as tf
 from numpy.random import randn
 from tensorflow.keras.layers import Input, BatchNormalization, Conv2D, LeakyReLU, GlobalAveragePooling2D, Dense
 from tensorflow.keras.losses import MAE
 from tensorflow.keras.models import Model
-from tensorflow.math import add
 
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(physical_devices[0], True)
@@ -28,7 +27,7 @@ def Darknet53():
     y = LeakyReLU(alpha=0.01)(BatchNormalization(axis=3)(y))
     y = Conv2D(64, (3, 3), padding='same', strides=(1, 1))(y)
     y = LeakyReLU(alpha=0.01)(BatchNormalization(axis=3)(y))
-    x = add([x, y])
+    x = tf.math.add([x, y])
 
     # 5
     x = Conv2D(128, (3, 3), padding='same', strides=(2, 2))(x)
@@ -43,7 +42,7 @@ def Darknet53():
     y = LeakyReLU(alpha=0.01)(BatchNormalization(axis=3)(y))
     y = Conv2D(128, (3, 3), padding='same', strides=(1, 1))(y)
     y = LeakyReLU(alpha=0.01)(BatchNormalization(axis=3)(y))
-    x = add([x, y])
+    x = tf.math.add([x, y])
 
     # 10
     x = Conv2D(256, (3, 3), padding='same', strides=(2, 2))(x)
@@ -82,7 +81,7 @@ def Darknet53():
     y = LeakyReLU(alpha=0.01)(BatchNormalization(axis=3)(y))
     y = Conv2D(256, (3, 3), padding='same', strides=(1, 1))(y)
     y = LeakyReLU(alpha=0.01)(BatchNormalization(axis=3)(y))
-    x = add([x, y])
+    x = tf.math.add([x, y])
 
     # 27
     x = Conv2D(512, (3, 3), padding='same', strides=(2, 2))(x)
@@ -121,7 +120,7 @@ def Darknet53():
     y = LeakyReLU(alpha=0.01)(BatchNormalization(axis=3)(y))
     y = Conv2D(512, (3, 3), padding='same', strides=(1, 1))(y)
     y = LeakyReLU(alpha=0.01)(BatchNormalization(axis=3)(y))
-    x = add([x, y])
+    x = tf.math.add([x, y])
 
     # 44
     x = Conv2D(1024, (3, 3), padding='same', strides=(2, 2))(x)
@@ -144,7 +143,7 @@ def Darknet53():
     y = LeakyReLU(alpha=0.01)(BatchNormalization(axis=3)(y))
     y = Conv2D(1024, (3, 3), padding='same', strides=(1, 1))(y)
     y = LeakyReLU(alpha=0.01)(BatchNormalization(axis=3)(y))
-    x = add([x, y])
+    x = tf.math.add([x, y])
 
     # 53
     # Global Mean Pooling layer
