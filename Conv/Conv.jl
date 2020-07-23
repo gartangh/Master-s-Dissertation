d33dffc8a8bf1c2c24907aa9e734d2eb2c9a7d64
Garben Tanghe <garben.tanghe@gmail.com>
2020-07-23 17:10:13 +0200

    finished

--- Conv/Conv.jl
+++ Conv/Conv.jl
@@ -1,4 +1,5 @@
 using Revise
+using NNlib
 using Flux
 using CUDA
 using BenchmarkTools
@@ -6,26 +7,33 @@ using BenchmarkTools
 DEVICE_ID = 0
 println(CUDA.name(CuDevice(DEVICE_ID)))
 
-# convolution = Conv((3, 3), 32 => 32, pad = (1, 1), stride = (1, 1)), x -> relu.(x) # relu separate
-# convolution = Conv((3, 3), 128 => 128, pad = (1, 1), stride = (1, 1)) # no activation
-convolution = Conv((3, 3), 128 => 128, relu, pad = (1, 1), stride = (1, 1)) # relu
+chain = Chain(
+            # Conv((3, 3), 128 => 128,         σ, pad = (1, 1), stride = (1, 1)),
+            Conv((3, 3), 128 => 128,      relu, pad = (1, 1), stride = (1, 1)),
+            # Conv((3, 3), 128 => 128,      tanh, pad = (1, 1), stride = (1, 1)),
+            # Conv((3, 3), 128 => 128,     trelu, pad = (1, 1), stride = (1, 1)),
+            # Conv((3, 3), 128 => 128,       elu, pad = (1, 1), stride = (1, 1)),
+            Conv((3, 3), 128 => 128,  identity, pad = (1, 1), stride = (1, 1)),
+            Conv((3, 3), 128 => 128,            pad = (1, 1), stride = (1, 1)),
+            Conv((3, 3), 128 => 128, leakyrelu, pad = (1, 1), stride = (1, 1)),
+)
 
 # extend Flux function
 function (c::Conv)(x::CuArray{T}) where T<:Union{Float16,Float32,Float64}
-  σ, b = c.σ, reshape(c.bias, ntuple(_->1, length(c.stride))..., :, 1)
-  cdims = DenseConvDims(x, c.weight; stride=c.stride, padding=c.pad, dilation=c.dilation)
-  # σ.(conv(x, c.weight, cdims) .+ b)
-  conv_bias_act(x, c.weight, cdims, b, σ)
+    σ, b = c.σ, reshape(c.bias, ntuple(_->1, length(c.stride))..., :, 1)
+    cdims = DenseConvDims(x, c.weight; stride=c.stride, padding=c.pad, dilation=c.dilation)
+    # σ.(conv(x, c.weight, cdims) .+ b)
+    conv_bias_act(x, c.weight, cdims, b, σ)
 end
 
 function fw(m, ip)
-    NVTX.@range "Conv CUDA.jl" begin
+    NVTX.@range "Chain CUDA.jl" begin
         CUDA.@sync m(ip)
     end
 end
 
 function benchmark_cudajl(batchsize)
-    m = convolution
+    m = chain
     ip = rand(Float32, 224, 224, 128, batchsize)
     GC.gc()
     CUDA.reclaim()
@@ -50,11 +58,11 @@ function benchmark_cudajl(batchsize)
         CUDA.reclaim()
     end
 
-    println()
+    println("DONE.")
 end
 
 function profile_cudajl(batchsize)
-    m = convolution
+    m = chain
     ip = rand(Float32, 224, 224, 128, batchsize)
     GC.gc()
     CUDA.reclaim()
@@ -72,4 +80,6 @@ function profile_cudajl(batchsize)
     CUDA.reclaim()
 
     CUDA.@profile fw(gm, gip)
+
+    println("DONE.")
 end
