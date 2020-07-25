ca17cd1956fd48f3d2492d955906bb17178191eb
Garben Tanghe <garben.tanghe@gmail.com>
2020-07-25 16:01:47 +0200

    faster softmax

---
+++ ConvBiasAct/Softmax.jl
@@ -0,0 +1,66 @@
+using Revise
+using Flux
+using NNlib
+using CUDA
+using BenchmarkTools
+
+DEVICE_ID = 0
+println(CUDA.name(CuDevice(DEVICE_ID)))
+
+function fw(m, ip)
+    NVTX.@range "Act CUDA.jl" begin
+        CUDA.@sync m(ip)
+    end
+end
+
+function benchmark_cudajl(batchsize)
+    m = softmax
+    ip = rand(Float32, 2<<18, batchsize)
+    GC.gc()
+    CUDA.reclaim()
+
+    gm = m |> gpu
+    gip = ip |> gpu
+
+    # warm-up
+    fw(gm, gip)
+    GC.gc()
+    CUDA.reclaim()
+
+    b = @benchmarkable(
+        fw($gm, $gip),
+        teardown = (GC.gc(); CUDA.reclaim())
+    )
+    display(run(b))
+
+    for _ in 1:5
+        CUDA.@time fw(gm, gip)
+        GC.gc()
+        CUDA.reclaim()
+    end
+
+    println()
+end
+
+function profile_cudajl(batchsize)
+    m = softmax
+    ip = rand(Float32, 2<<18, batchsize)
+    GC.gc()
+    CUDA.reclaim()
+
+    gm = m |> gpu
+    gip = ip |> gpu
+
+    # warm-up
+    fw(gm, gip)
+    GC.gc()
+    CUDA.reclaim()
+
+    CUDA.@time fw(gm, gip)
+    GC.gc()
+    CUDA.reclaim()
+
+    CUDA.@profile fw(gm, gip)
+
+    println()
+end
