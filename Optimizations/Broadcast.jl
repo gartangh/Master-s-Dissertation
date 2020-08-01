86785906acc45c87ab1ef5c3390e65290ee7fd3a
Garben Tanghe <garben.tanghe@gmail.com>
2020-08-01 23:01:56 +0200

    added additional broadcast tests

--- Optimizations/Broadcast.jl
+++ Optimizations/Broadcast.jl
@@ -30,6 +30,14 @@ config5 = Chain(
             x -> relu.(x),
 )
 
+config6 = Chain(
+            x -> identity(x),
+)
+
+config7 = Chain(
+            x -> identity.(x),
+)
+
 # extend Flux function
 function (c::Conv)(x::CuArray{T}) where T<:Union{Float16,Float32,Float64}
     σ, b = c.σ, reshape(c.bias, ntuple(_->1, length(c.stride))..., :, 1)
@@ -68,72 +76,27 @@ function fw5(m, ip)
     end
 end
 
-function benchmark_cudajl(batchsize)
-    m1 = config1
-    m2 = config2
-    m3 = config3
-    m4 = config4
-    m5 = config5
-    ip = rand(Float32, 224, 224, 128, batchsize)
-    GC.gc()
-    CUDA.reclaim()
-
-    gm1 = m1 |> gpu
-    gm2 = m2 |> gpu
-    gm3 = m3 |> gpu
-    gm4 = m4 |> gpu
-    gm5 = m5 |> gpu
-    gip = ip |> gpu
-
-    # warm-up
-    fw1(gm1, gip)
-    fw2(gm2, gip)
-    fw3(gm3, gip)
-    fw4(gm4, gip)
-    fw5(gm5, gip)
-    GC.gc()
-    CUDA.reclaim()
-
-    b = @benchmarkable(
-        fw1($gm1, $gip),
-        teardown = (GC.gc(); CUDA.reclaim())
-    )
-    display(run(b))
-
-    b = @benchmarkable(
-        fw2($gm2, $gip),
-        teardown = (GC.gc(); CUDA.reclaim())
-    )
-    display(run(b))
-
-    b = @benchmarkable(
-        fw3($gm3, $gip),
-        teardown = (GC.gc(); CUDA.reclaim())
-    )
-    display(run(b))
-
-    b = @benchmarkable(
-        fw4($gm4, $gip),
-        teardown = (GC.gc(); CUDA.reclaim())
-    )
-    display(run(b))
-
-    b = @benchmarkable(
-        fw5($gm5, $gip),
-        teardown = (GC.gc(); CUDA.reclaim())
-    )
-    display(run(b))
-
-    println()
+function fw6(m, ip)
+    NVTX.@range "config6 CUDA.jl" begin
+        CUDA.@sync m(ip)
+    end
 end
 
-function profile_cudajl(batchsize)
+function fw7(m, ip)
+    NVTX.@range "config7 CUDA.jl" begin
+        CUDA.@sync m(ip)
+    end
+end
+
+function main()
     m1 = config1
     m2 = config2
     m3 = config3
     m4 = config4
     m5 = config5
-    ip = rand(Float32, 224, 224, 128, batchsize)
+    m6 = config6
+    m7 = config7
+    ip = rand(Float32, 224, 224, 128, 16)
     GC.gc()
     CUDA.reclaim()
 
@@ -142,6 +105,8 @@ function profile_cudajl(batchsize)
     gm3 = m3 |> gpu
     gm4 = m4 |> gpu
     gm5 = m5 |> gpu
+    gm6 = m6 |> gpu
+    gm7 = m7 |> gpu
     gip = ip |> gpu
 
     # warm-up
@@ -150,6 +115,8 @@ function profile_cudajl(batchsize)
     fw3(gm3, gip)
     fw4(gm4, gip)
     fw5(gm5, gip)
+    fw6(gm6, gip)
+    fw7(gm7, gip)
     GC.gc()
     CUDA.reclaim()
 
@@ -158,14 +125,12 @@ function profile_cudajl(batchsize)
     CUDA.@time fw3(gm3, gip)
     CUDA.@time fw4(gm4, gip)
     CUDA.@time fw5(gm5, gip)
+    CUDA.@time fw6(gm6, gip)
+    CUDA.@time fw7(gm7, gip)
     GC.gc()
     CUDA.reclaim()
 
-    CUDA.@profile fw1(gm1, gip)
-    CUDA.@profile fw2(gm2, gip)
-    CUDA.@profile fw3(gm3, gip)
-    CUDA.@profile fw4(gm4, gip)
-    CUDA.@profile fw5(gm5, gip)
-
     println()
 end
+
+main()
